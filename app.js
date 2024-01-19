e535f5c993df3fc2949c25cf8970289e496236f8
Leonardo De Moura Souza <Leo.estudos12@gmail.com>
2024-01-19 14:20:24 -0300

    novo site

---
+++ app.js
@@ -0,0 +1,25 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10')
+    // se o chute for igual ao número secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+    } else {
+            alert(`O número secreto é maior que ${chute}`);
+    }
+    //tentativas = tentativas +1
+    tentativas++;
+}
+}
+    
+
+    
